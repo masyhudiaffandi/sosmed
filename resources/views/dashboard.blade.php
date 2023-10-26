5077a98b17def3e24868033a4e0a759e01d94210
masyhudiaffandi <masyhudiaffandi816@gmail.com>
2023-10-26 22:44:27 +0700

    first commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,10 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                    <form action="/tweets" class="form-control" method="post">
+                        <textarea name="content" id="" cols="30" rows="3" class="textarea textarea-primary mb-4" placeholder="tulis sesuatu"></textarea>
+                        <input type="submit" class="btn btn-primary" value="tweet">
+                    </form>
                 </div>
             </div>
         </div>
